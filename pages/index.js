5778728ce72fdecce389cf6535e5686c380f38a5
Andrew McAuliffe <andrew.mcauliffe@gmail.com>
2022-07-24 12:26:01 -0500

    Create rough initial UI

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('');
+  const [definition, setDefinition] = useState(undefined);
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    fetch('http://localhost:8081/getDefinition/' + inputValue)
+      .then(res => res.json())
+      .then(result => setDefinition(result.word));
+    event.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,57 +28,21 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Enter an acronym:
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <p className={styles.description}>
+          {definition}
+        </p>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        Built with 🍺 in Birmingham, Ala.
       </footer>
     </div>
   )
